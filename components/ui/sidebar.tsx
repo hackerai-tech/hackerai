68b002c53d5646111ce391565990f09421797104
Rostyslav Manko <contact@hackerai.co>
2025-09-03 15:10:11 -0400

    refactor: improve sidebar trigger

--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -302,14 +302,37 @@ function SidebarRail({ className, ...props }: React.ComponentProps<"button">) {
       onClick={toggleSidebar}
       title="Toggle Sidebar"
       className={cn(
-        "hover:after:bg-sidebar-border absolute inset-y-0 z-20 hidden w-4 -translate-x-1/2 transition-all ease-linear group-data-[side=left]:-right-4 group-data-[side=right]:left-0 after:absolute after:inset-y-0 after:left-1/2 after:w-[2px] sm:flex",
+        "absolute inset-y-0 z-20 hidden w-4 -translate-x-1/2 transition-all ease-linear group-data-[side=left]:-right-4 group-data-[side=right]:left-0 sm:flex",
         "in-data-[side=left]:cursor-w-resize in-data-[side=right]:cursor-e-resize",
         "[[data-side=left][data-state=collapsed]_&]:cursor-e-resize [[data-side=right][data-state=collapsed]_&]:cursor-w-resize",
-        "hover:group-data-[collapsible=offcanvas]:bg-sidebar group-data-[collapsible=offcanvas]:translate-x-0 group-data-[collapsible=offcanvas]:after:left-full",
+        "hover:group-data-[collapsible=offcanvas]:bg-sidebar group-data-[collapsible=offcanvas]:translate-x-0",
         "[[data-side=left][data-collapsible=offcanvas]_&]:-right-2",
         "[[data-side=right][data-collapsible=offcanvas]_&]:-left-2",
         className,
       )}
+      {...props}
+    />
+  );
+}
+
+function SidebarExpandArea({ className, ...props }: React.ComponentProps<"div">) {
+  const { toggleSidebar, state, isMobile } = useSidebar();
+
+  // Only show on non-mobile when collapsed
+  if (isMobile || state !== "collapsed") {
+    return null;
+  }
+
+  return (
+    <div
+      data-sidebar="expand-area"
+      data-slot="sidebar-expand-area"
+      className={cn(
+        "absolute inset-0 z-10 cursor-e-resize hover:bg-sidebar-accent/5 transition-colors",
+        className,
+      )}
+      onClick={toggleSidebar}
+      title="Click to expand sidebar"
       {...props}
     />
   );
@@ -385,11 +408,14 @@ function SidebarContent({ className, ...props }: React.ComponentProps<"div">) {
       data-slot="sidebar-content"
       data-sidebar="content"
       className={cn(
-        "flex min-h-0 flex-1 flex-col gap-2 overflow-auto group-data-[collapsible=icon]:overflow-hidden",
+        "relative flex min-h-0 flex-1 flex-col gap-2 overflow-auto group-data-[collapsible=icon]:overflow-hidden",
         className,
       )}
       {...props}
-    />
+    >
+      {props.children}
+      <SidebarExpandArea />
+    </div>
   );
 }
 
@@ -712,6 +738,7 @@ function SidebarMenuSubButton({
 export {
   Sidebar,
   SidebarContent,
+  SidebarExpandArea,
   SidebarFooter,
   SidebarGroup,
   SidebarGroupAction,
