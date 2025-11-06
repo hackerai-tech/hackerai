819775f14a6e7333f925b372917e904627ef9ad2
Foad Kesheh <foad.kesheh@shippo.com>
2025-11-06 11:34:27 -0300

    refactor: remove QueuedMessage interface from types
    Remove leftover QueuedMessage interface that was part of the message
    queue feature which was removed from this testing infrastructure PR

--- types/chat.ts
+++ types/chat.ts
@@ -95,14 +95,3 @@ export type RateLimitInfo = {
   resetTime: Date;
   limit: number;
 };
-
-export interface QueuedMessage {
-  id: string;
-  text: string;
-  files?: Array<{
-    file: File;
-    fileId: string;
-    url: string;
-  }>;
-  timestamp: number;
-}
