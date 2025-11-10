9533d1506e8f5ad648096499a27ecc9166b603ad
Rostyslav Manko <contact@hackerai.co>
2025-11-10 13:27:07 -0500

    refactor(e2b-template): add more pentest tools

--- e2b/template.ts
+++ e2b/template.ts
@@ -10,16 +10,17 @@ export const template = Template()
       git \
       ca-certificates \
       gnupg \
-      nmap \
       iputils-ping \
       whois \
       traceroute \
       dnsutils \
+      net-tools \
+      pandoc \
+      libpcap-dev \
+      nmap \
       whatweb \
       wafw00f \
-      net-tools \
-      gobuster \
-      pandoc && \
+      sqlmap && \
     apt-get autoremove -y && \
     apt-get clean && \
     rm -rf /var/lib/apt/lists/* && \
@@ -39,11 +40,20 @@ export const template = Template()
       python-pptx \
       pandas \
       pypandoc \
-      odfpy",
+      odfpy \
+      arjun \
+      dirsearch",
     { user: "root" },
   )
   .runCmd(
     "git clone --depth 1 https://github.com/danielmiessler/SecLists.git /home/user/SecLists",
+    { user: "root" },
+  )
+  .runCmd(
+    "git clone https://github.com/ticarpi/jwt_tool.git /opt/jwt_tool && \
+    chmod +x /opt/jwt_tool/jwt_tool.py && \
+    pip install --no-cache-dir -r /opt/jwt_tool/requirements.txt && \
+    ln -s /opt/jwt_tool/jwt_tool.py /usr/local/bin/jwt_tool",
     { user: "root" },
   )
   .setEnvs({
@@ -67,25 +77,37 @@ export const template = Template()
   )
   .runCmd(
     "go install github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest && \
-    ln -s /go/bin/subfinder /usr/local/bin/subfinder",
+    go install github.com/projectdiscovery/httpx/cmd/httpx@latest && \
+    go install github.com/projectdiscovery/naabu/v2/cmd/naabu@latest && \
+    go install github.com/OJ/gobuster/v3@latest && \
+    ln -s /go/bin/httpx /usr/local/bin/httpx && \
+    ln -s /go/bin/naabu /usr/local/bin/naabu && \
+    ln -s /go/bin/subfinder /usr/local/bin/subfinder && \
+    ln -s /go/bin/gobuster /usr/local/bin/gobuster",
     { user: "root" },
   )
   .runCmd(
     'test -d /home/user/SecLists && \
     test -f /home/user/SecLists/README.md && \
-    nmap --version && \
     whois --version && \
     traceroute --version && \
     dig -v && \
     go version && \
-    whatweb --version && \
-    wafw00f --version && \
     ping -V && \
     ifconfig -v && \
-    gobuster -v && \
-    subfinder --version && \
     pandoc --version && \
-    python -c "import reportlab; import docx; import openpyxl; import pptx; import pandas; import pypandoc; import odf"',
+    python -c "import reportlab; import docx; import openpyxl; import pptx; import pandas; import pypandoc; import odf" && \
+    nmap --version && \
+    httpx --version && \
+    naabu --version && \
+    subfinder --version && \
+    gobuster --version && \
+    sqlmap --version && \
+    whatweb --version && \
+    wafw00f --version && \
+    arjun -h && \
+    dirsearch --version && \
+    jwt_tool -h',
     { user: "root" },
   )
   .setWorkdir("/home/user");
